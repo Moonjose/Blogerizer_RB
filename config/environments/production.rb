ad5880a911933535733c8be8ed3d4f7a9fe2c897
Jose <ajose9975@gmail.com>
2022-07-15 19:14:40 -0300

    Fix deploy bug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -125,7 +125,7 @@ Rails.application.configure do
   address: 'smtp.gmail.com',
   port: 587,
   domain: 'gmail.com',
-  user_name: Rails.application.credentials.gmail[:username].to_s,
+  user_name: Rails.application.credentials.gmail[:user_name].to_s,
   password: Rails.application.credentials.gmail[:password],
   authentication: 'plain',
   enable_starttls_auto: true,
