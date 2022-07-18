7714c0ee41356eb8d76e1b2ab8015a6666f9937b
Jose <ajose9975@gmail.com>
2022-07-18 20:01:28 -0300

    Fixed some bugs

--- app/models/article.rb
+++ app/models/article.rb
@@ -7,7 +7,7 @@ class Article < ApplicationRecord
   validates :title, presence: true, length: {minimum: 5}
   validates :body, presence: true, length: {minimum: 10}
 
-  paginates_per 2
+  paginates_per 5
 
   scope :desc_order, -> { order(created_at: :desc) }
   scope :without_highlights, -> (ids) {where("id NOT IN(#{ids})") if ids.present? }
